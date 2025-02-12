2a6a8ea664dba77c6e8db117e33b3771e03a3da0
K758-hoho <karinaheryono22@gmail.com>
2025-02-12 13:15:07 +0700

    Added .eleventy.js and index.html

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+    // Existing configuration...
+  
+    return {
+      dir: {
+        input: "_site",
+        output: "docs",
+      },
+    };
+  };
