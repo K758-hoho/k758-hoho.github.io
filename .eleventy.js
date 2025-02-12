94ecdab2e48f42f5fd16459c717351fda33c9db1
karin-kho <137480456+K758-hoho@users.noreply.github.com>
2025-02-12 19:55:47 +0700

    Did a lot of stuff...

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
     // Pass through static files
-    eleventyConfig.addPassthroughCopy("src/assets"); // Adjust this if you have static assets
+    eleventyConfig.addPassthroughCopy("src/assets"); //Folder for static assets like images, css, js
+    eleventyConfig.addPassthroughCopy("src/posts"); // Folder for blog posts
 
     // Add a filter to format dates
     eleventyConfig.addFilter("date", (dateObj, format) => {
@@ -14,9 +15,15 @@ module.exports = function (eleventyConfig) {
         return DateTime.now().toFormat("yyyy");
     });
 
+    // Add a filter to get the post time
+    eleventyConfig.addFilter("post-time", (dateObj) => {
+        return DateTime.fromJSDate(new Date(dateObj)).toFormat("HH:mm:yyyy");
+    });
+
     return {
       dir: {
         input: "src",
+        includes"_includes",
         output: "docs",
       },
       htmlTemplateEngine: "njk",
