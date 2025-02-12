460bee6ffdb9404084139619323be46a47ecef61
K758-hoho <karinaheryono22@gmail.com>
2025-02-12 17:02:27 +0700

    Added an 11ty filter to track current year

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,26 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
     // Pass through static files
     eleventyConfig.addPassthroughCopy("src/assets"); // Adjust this if you have static assets
+
+    // Add a filter to format dates
+    eleventyConfig.addFilter("date", (dateObj, format) => {
+        return DateTime.fromJSDate(new Date(dateObj)).toFormat(format);
+    });
+
+    // Add a filter to get the current year
+    eleventyConfig.addFilter("year", () => {
+        return DateTime.now().toFormat("yyyy");
+    });
 
     return {
       dir: {
         input: "src",
         output: "docs",
       },
+      htmlTemplateEngine: "njk",
+      markdownTemplateEngine: "njk",
+      dataTemplateEngine: 'njk',
     };
 };
