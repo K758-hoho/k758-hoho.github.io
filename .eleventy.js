42f54650dee966f12fdbb67b22f1700cf579d31a
K758-hoho <karinaheryono22@gmail.com>
2025-02-21 16:22:06 +0700

    Added collections and plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,46 @@
+// Purpose: Eleventy configuration file
+// Documentation: https://www.11ty.dev/docs/config/
+
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation"
+import pluginRss from "@11ty/eleventy-plugin-rss"
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img"
+
+import {
+  getAllPosts,
+  getCategoryList,
+  getCategorisedPosts,
+} from "/src/config/collections.js"
+
 const { DateTime } = require("luxon");
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
     // Install plugins
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(pluginRss);
-    
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
     // Pass through static files
     eleventyConfig.addPassthroughCopy("src/assets/images"); // Folder for images
     eleventyConfig.addPassthroughCopy("src/assets/css"); // Folder for css
     eleventyConfig.addPassthroughCopy("src/assets/posts"); // Folder for blog posts
 
-    // Add a filter to format dates, e.g. {{ post.date | date("dd LLLL yyyy") }}
-    eleventyConfig.addFilter("date", (dateObj, format) => {
-        return DateTime.fromJSDate(new Date(dateObj)).toFormat(format);
-    });
+    // Add collections
+    eleventyConfig.addCollection("blog", getAllPosts)
+    eleventyConfig.addCollection("categoryList", getCategoryList)
+    eleventyConfig.addCollection("categorisedPosts", getCategorisedPosts)
+
+    // Add layour aliases
+    eleventyConfig.addLayoutAlias("page", "layouts/page")
+    eleventyConfig.addLayoutAlias("article", "layouts/article")
+
+    // Add date parsing, e.g. {{ page.date | dateToFormat("yyyy-MM-dd hh:mm:ss location") }} or 2019-08-31 23:59:56 America/New_York
+    eleventyConfig.addDateParsing(function(dateValue) {
+		return DateTime.fromFormat(dateValue, "yyyy-MM-dd hh:mm:ss z");
+	});
 
     // Add a filter to get the current year, e.g. {{ "now" | year }}
     eleventyConfig.addFilter("year", () => {
         return DateTime.now().toFormat("yyyy");
-    });
-
-    // Add a filter to get the date of a post, e.g. {{ post.date | post-time }}
-    eleventyConfig.addFilter("post-time", (dateObj) => {
-        return DateTime.fromJSDate(new Date(dateObj)).toFormat("HH:mm:yyyy");
     });
 
     // Add a filter to randomize items, e.g. {% for fact in facts | randomItem %}
