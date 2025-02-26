a4bb1d2e0adc22bec475015eeeec71d1866bf118
K758-hoho <karinaheryono22@gmail.com>
2025-02-26 08:17:06 +0700

    edited layouts

--- .eleventy.js
+++ .eleventy.js
@@ -1,70 +1,50 @@
-// Purpose: Eleventy configuration file
-// Documentation: https://www.11ty.dev/docs/config/
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const readingTime = require('eleventy-plugin-reading-time');
 
-import eleventyNavigationPlugin from "@11ty/eleventy-navigation"
-import pluginRss from "@11ty/eleventy-plugin-rss"
+const getAllPosts = require('./src/config/collections').getAllPosts;
+const getCategoryList = require('./src/config/collections').getCategoryList;
+const getCategorisedPosts = require('./src/config/collections').getCategorisedPosts;
 
-import {
-  getAllPosts,
-  getCategoryList,
-  getCategorisedPosts,
-} from "./src/config/collections.js"
+const DateTime = require('luxon').DateTime;
 
-import { DateTime } from "luxon";
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(readingTime);
 
-export default function (eleventyConfig) {
-    // Install plugins
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
-    eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addLayoutAlias("article", "layouts/article");
+  eleventyConfig.addLayoutAlias("post", "layouts/post");
+  eleventyConfig.addLayoutAlias("home", "layouts/home");
 
-    // Pass through static files
-    eleventyConfig.addPassthroughCopy("src/assets/images"); // Folder for images
-    eleventyConfig.addPassthroughCopy("src/assets/css"); // Folder for css
-    eleventyConfig.addPassthroughCopy("src/assets/posts"); // Folder for blog posts
+  eleventyConfig.addDateParsing(function(dateValue) {
+    if (typeof dateValue === 'string') {
+      return DateTime.fromFormat(dateValue, "yyyy-MM-dd hh:mm:ss z");
+    } else {
+      return null;
+    }
+  });
 
-    // Add collections
-    eleventyConfig.addCollection("blog", getAllPosts)
-    eleventyConfig.addCollection("categoryList", getCategoryList)
-    eleventyConfig.addCollection("categorisedPosts", getCategorisedPosts)
+  eleventyConfig.addFilter("year", function() {
+    return DateTime.now().toFormat("yyyy");
+  });
 
-    // Add layout aliases
-    eleventyConfig.addLayoutAlias("page", "layouts/page")
-    eleventyConfig.addLayoutAlias("article", "layouts/article")
-    eleventyConfig.addLayoutAlias("post", "layouts/post")
-    eleventyConfig.addLayoutAlias("home", "layouts/home")
-
-    eleventyConfig.addDateParsing(function(dateValue) {
-        if (typeof dateValue === 'string') {
-          return DateTime.fromFormat(dateValue, "yyyy-MM-dd hh:mm:ss z");
-        } else {
-          // Handle the case where dateValue is not a string
-          // You can return a default value or throw an error, depending on your needs
-          return null;
-        }
-      });
-
-    // Add a filter to get the current year, e.g. {{ "now" | year }}
-    eleventyConfig.addFilter("year", () => {
-        return DateTime.now().toFormat("yyyy");
+  eleventyConfig.addFilter("randomItem", function(arr) {
+    arr.sort(function() {
+      return 0.5 - Math.random();
     });
+    return arr.slice(0, 1);
+  });
 
-    // Add a filter to randomize items, e.g. {% for fact in facts | randomItem %}
-    eleventyConfig.addFilter("randomItem", (arr) => {
-        arr.sort(() => {
-            return 0.5 - Math.random();
-        });
-        return arr.slice(0, 1);
-    });
-
-    return {
-        dir: {
-            input: "src",
-            includes: "_includes",
-            output: "docs",
-        },
-        htmlTemplateEngine: "njk",
-        markdownTemplateEngine: "njk",
-        dataTemplateEngine: 'njk',
-        templateFormats: ["njk", "md"]
-    };
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "docs",
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: 'njk',
+    templateFormats: ["njk", "md"]
+  };
 };
